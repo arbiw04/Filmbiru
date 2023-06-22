c7844d51f0c584caf54ffb6d80eb6acaacd4c044
arbiw04 <50599107+arbiw04@users.noreply.github.com>
2023-06-22 15:42:49 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
+					endpoint.replace(/(\/graphql\/)/, '/') + encodeURI(path as string)
 				}`,
 			},
 		};
